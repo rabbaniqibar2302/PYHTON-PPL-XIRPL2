032430d268083534eedcff1741bc951a43d40243
Muhammad Rizqi Akbar Rabbani <34941031+rabbaniqibar2302@users.noreply.github.com>
2022-12-05 07:13:47 +0700

    latihan dan projek lengkap
    jangan lupa nilai saya jadiin A ya pak wkwkw

---
+++ biasa.php
@@ -0,0 +1,51 @@
+<html>
+    <head>
+        <title>Kalkulator Biasa</title>
+    </head>
+    <body>
+        <h1>Kalkulator Biasa</h1>
+        <form>  <!-- Bersifat GET secara default tanpa harus method -->
+            <input type="number" name="angka1">
+            <select name="operator">
+                <option selected disabled>Pilih operator</option>
+                <option value="+">+</option>
+                <option value="-">-</option>
+                <option value="*">*</option>
+                <option value="/">/</option>
+            </select>
+            <input type="number" name="angka2">
+            <div style="margin-top:10px;">
+                <button type="reset">Clear</button>
+                <button type="submit">Hitung</button>
+            </div>
+            <!-- Logika Kalkulator Biasa -->
+            <?php
+            if ($_GET):
+                $angka1 = (double) @$_GET['angka1'];
+                $angka2 = (double) @$_GET['angka2'];
+                $operator = @$_GET['operator'];
+                // Buat logika untuk penjumlahan
+                switch ($operator){
+                    case '+':
+                        $hasil = $angka1 + $angka2;
+                        break;
+                    case '-':
+                        $hasil = $angka1 - $angka2;
+                        break;
+                    case '*':
+                        $hasil = $angka1 * $angka2;
+                        break;
+                    case '/':
+                        $hasil = $angka1 / $angka2;
+                        break;
+                }
+            ?>
+            <div style="margin-top: 10px">
+                HASIL : <strong><?php echo $hasil ?></strong>
+            </div>
+            <?php
+            endif; ?>
+            <!-- End Logika -->
+        </form>
+    </body>
+</html>
